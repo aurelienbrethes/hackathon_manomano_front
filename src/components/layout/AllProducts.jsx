4b8545e2579d622a2e3478a692bd360c8ca2dfd5
DavidVM19 <davidvm1910@gmail.com>
2022-01-21 00:11:49 +0100

    add some details AllProducts

--- src/components/layout/AllProducts.jsx
+++ src/components/layout/AllProducts.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
+import Filters from '../../ressources/filter.png';
+import Mpro from '../../ressources/mpro.png';
 
 const AllProducts = () => {
     const [allItems, setAllItems] = useState([]);
@@ -8,24 +10,29 @@ const AllProducts = () => {
         axios.get(`http://localhost:8000/api/products`).then((res) => setAllItems(res.data));
     }, []);
 
-    console.log(allItems)
     return (
         <div className="allProducts">
             <p className="allProducts__page">Accueil {" > "} Outillage {" > "} Outillage électroportatif {" > "} <b>Perceuse, visseuse, perforateur, burineur, marteau-piqueur</b></p>
             <p className="allProducts__infoSearch">Perceuse, visseuse, perforateur, burineur, marteau-piqueur</p>
             <p className="allProducts__numberSearch">Plus de 500 produits trouvés</p>
-                <div className="allProducts__list">
-                {allItems.map((item, index) => {
-                    return (
-                        <div className="allProducts__list__details" key={index}>
-                            <div className="allProducts__list__details__picture"><img src={item.img} alt={item.name} /></div>
-                            <div className="allProducts__list__details__brand">{item.brand !== "" ? <img src={item.brand} alt="marque" /> : ""}</div>
-                            <div className="allProducts__list__details__name"><p>{item.name}</p></div>
-                            <div className="allProducts__list__details__price">{item.price} € HT</div>
-                        </div>
-                    )
-                })}
+            <div className="allProducts__body">
+                <img className="allProducts__body__filters" src={Filters}/>
+                <div className="allProducts__body__list">
+                    {allItems.map((item, index) => {
+                        return (
+                            <div className="allProducts__body__list__details" key={index}>
+                                <div className="allProducts__body__list__details__picture"><img src={item.img} alt={item.name} /></div>
+                                <div className="allProducts__body__list__details__brand">{item.brand !== "" ? <img src={item.brand} alt="marque" /> : ""}</div>
+                                <div className="allProducts__body__list__details__name"><p>{item.name}</p></div>
+                                <div className="allProducts__body__list__details__price">
+                                    <div className="allProducts__body__list__details__price__HT">{item.price}<p>€00 HT</p></div>
+                                    <div className="allProducts__body__list__details__price__TTC">{item.price * 20 / 100 + item.price} € TTC <img src={Mpro} alt="Mpro"/></div>
+                                </div>
+                            </div>
+                        )
+                    })}
                 </div>
+            </div>
         </div>
     )
 }
