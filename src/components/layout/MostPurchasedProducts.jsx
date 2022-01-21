9654421882ace39edc77dd16a77003c8d1ca54a8
Matthieu Dubo <matthieu.dubo40@gmail.com>
2022-01-21 02:24:36 +0100

    modif

--- src/components/layout/MostPurchasedProducts.jsx
+++ src/components/layout/MostPurchasedProducts.jsx
@@ -12,6 +12,8 @@ const MostPurchasedProducts = () => {
       .then((data) => setPurchasedProducts(data));
   }, []);
 
+  console.log(purchasedProducts);
+
   return (
     <div className="most-purchased-products">
       <div className="most-purchased-products__title">
