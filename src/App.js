75425a3acb282b823095992e491556600bef6e28
marie5060 <marie5060hotmail.com>
2022-01-21 01:35:06 +0100

    Conflict in App about Home being named home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar/Navbar";
 import MostPurchasedProducts from "./components/layout/MostPurchasedProducts";
-import Home from "./components/pages/home";
+import Home from "./components/pages/Home";
 import Orders from "./components/layout/Orders";
 import Footer from "./components/layout/Footer";
 import AllProducts from "./components/layout/AllProducts";
