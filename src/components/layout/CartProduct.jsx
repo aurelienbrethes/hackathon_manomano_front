d5b7044d574e068e2b01e3767e29cbad2721cdbb
Jimmy <jimmy.ganci@gmail.com>
2022-01-21 01:07:16 +0100

    added qty button on productCard

--- src/components/layout/CartProduct.jsx
+++ src/components/layout/CartProduct.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import plus from "../../ressources/plus.png";
+import moins from "../../ressources/moins.png";
 
 const CartProduct = ({ name, price, img, quantity, setQuantity }) => {
+  const [qte, setQte] = useState(0);
+  console.log(qte);
   return (
     <div className="cart-product">
       <div className="cart-product__main">
@@ -18,11 +22,25 @@ const CartProduct = ({ name, price, img, quantity, setQuantity }) => {
         </div>
       </div>
       <div className="cart-product__qtyprice">
-        <input
-          type="number"
-          value={quantity}
-          onChange={(e) => setQuantity(e.target.value)}
-        ></input>
+        <div className="cart-product__qtyprice__spinner">
+          <button
+            className="cart-product__qtyprice__spinner--left"
+            onClick={() => qte > 0 && setQte(qte - 1)}
+          >
+            <img src={moins} alt="" />
+          </button>
+          <input
+            type="number"
+            value={qte}
+            onChange={(e) => setQte(e.target.value)}
+          />
+          <button
+            className="cart-product__qtyprice__spinner--right"
+            onClick={() => setQte(qte + 1)}
+          >
+            <img src={plus} alt="" />
+          </button>
+        </div>
         <div>
           <p className="cart-product__priceTTC">
             {Math.round(price * 1.2)} TTC
